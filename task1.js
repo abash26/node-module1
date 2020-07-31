86da5fba547f92656e57f9fd36aaaab2256a8189
Nastya <anastasiiabashahurova@gmail.com>
2020-07-31 11:32:25 +0200

    task 1

---
+++ task1.js
@@ -0,0 +1,14 @@
+/*const readline = require('readline')
+
+readline
+  .createInterface({
+    input: process.stdin,
+    output: process.stdout,
+    terminal: false,
+  })
+  .on('line', (line) => console.log(line.split('').reverse().join(''))) */
+
+process.stdin.on('data', (chunk) => {
+  let data = String(chunk).replace('\r\n', '').split('').reverse().join('')
+  process.stdout.write(data + '\n')
+})
