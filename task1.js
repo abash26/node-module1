afab41b6864e1f71bb033e4c047e056745e26d64
Nastya <anastasiiabashahurova@gmail.com>
2020-07-31 15:28:55 +0200

    task 1 pipe fix

--- task1.js
+++ task1.js
@@ -1,20 +1,3 @@
-/*
-const readline = require('readline')
-
-readline
-  .createInterface({
-    input: process.stdin,
-    output: process.stdout,
-    terminal: false,
-  })
-  .on('line', (line) => console.log(line.split('').reverse().join(''))) 
-
-process.stdin.on('data', (chunk) => {
-  let data = String(chunk).replace('\r\n', '').split('').reverse().join('')
-  process.stdout.write(data + '\n')
-})
-*/
-
 const { Transform } = require('stream')
 
 const dataTransform = new Transform({
@@ -27,3 +10,20 @@ const dataTransform = new Transform({
 })
 
 process.stdin.pipe(dataTransform).pipe(process.stdout)
+
+// Option 2
+/* const readline = require('readline')
+
+readline
+  .createInterface({
+    input: process.stdin,
+    output: process.stdout,
+    terminal: false,
+  })
+  .on('line', (line) => console.log(line.split('').reverse().join(''))) 
+
+// Option 3
+process.stdin.on('data', (chunk) => {
+  let data = String(chunk).replace('\r\n', '').split('').reverse().join('')
+  process.stdout.write(data + '\n')
+}) */
